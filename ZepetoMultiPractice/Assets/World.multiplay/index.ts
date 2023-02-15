16ab53560b3a1136b51f9f439b898941b474bb6d
heeyoung <hhllldpei@gmail.com>
2023-02-16 01:36:59 +0900

    index.ts 기초 수정

--- ZepetoMultiPractice/Assets/World.multiplay/index.ts
+++ ZepetoMultiPractice/Assets/World.multiplay/index.ts
@@ -3,7 +3,16 @@ import { Sandbox, SandboxOptions, SandboxPlayer } from "ZEPETO.Multiplay";
 export default class extends Sandbox {
 
     onCreate(options: SandboxOptions) {
-        
+        console.log("OnCreate!!!!");
+        this.onMessage?.("echo", (client, message) => {
+            console.log(`Echo onMessage from ${client.sessionId}, -> ${message}`);
+
+            // Send current client
+            client.send("echo", "echo to sender : " + message);
+
+            // Broadcast all connected client
+            this.broadcast?.("echo", "echo to all : " + message);
+        });
     }
 
     onJoin(client: SandboxPlayer) {
